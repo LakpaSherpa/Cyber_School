226be0e06747959195c0f32f4f055a8a169b322d
Pasang Lakpa Shrepa <pasanglakpa101@gmail.com>
2020-07-05 13:10:09 +0545

    System error handler created

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ var myapp = new express();
 
 
 
+
+
+// system error handler
+myapp.use(function (err, req, res, next) {
+    console.log(err);
+    res.status(err.status);
+    res.send({
+        "message": err.message
+    })
+})
+
+
 // set port
 myapp.listen(3000);
 
