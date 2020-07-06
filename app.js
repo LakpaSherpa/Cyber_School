98e309a0345079fb11a6148a7365eb46b1632741
Pasang Lakpa Shrepa <pasanglakpa101@gmail.com>
2020-07-06 13:48:36 +0545

    Path and multers are initialized

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 var express = require('express');
 var myapp = new express();
 var bodyParser = require('body-parser');
-
+var path = require('path');
+var multer = require('multer');
 
 // bodyParser
 myapp.use(bodyParser.json());
 myapp.use(bodyParser.urlencoded({ extended: true }));
+
+// path
+myapp.use(express.static(
+    path.join(__dirname, '/resources')
+));
+
+
+
+
+// multer storage
+var mystorage = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+        cb(null, 'resources/videos/courses')
+    },
+
+    destination: function (req, file, cb) {
+        cb(null, 'resources/videos/profileImage')
+
+    },
+    filename: function (req, file, cb) {
+        var name = 'asdasd' + (Math.floor(100000 + Math.random() * 900000)) + file.originalname;
+        cb(null, name);
+        req.testVall = name;
+    }
+});
+
+
+var upload = multer({ storage: mystorage });
+
+
+//profile Image upload
+var mystoragee = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'resources/videos/profileImage')
+    },
+    filename: function (req, file, cb) {
+        var name = 'asdasd' + (Math.floor(100000 + Math.random() * 900000)) + file.originalname;
+        cb(null, name);
+        req.testVall = name;
+    }
+});
+
+
 
 
 //this is the first middleware - application middleware , all routes hit this middleware first
