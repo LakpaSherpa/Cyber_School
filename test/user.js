a1561d93b29b7aa9b3cde20798b8bd52bd9768d1
Santosh Maharjan <santosh485288@gmail.com>
2020-08-15 19:35:34 +0545

    register student

--- test/user.js
+++ test/user.js
@@ -65,3 +65,31 @@ describe('admin login', () => {
         });
     });
 });
+
+
+
+// Register student
+describe('Students', function() {
+    describe('POST student register', function() {
+        it('it should register a single student', function(done) {
+            chai.request(myapp)
+                .post('/student/register')
+                .send({
+                    "FirstName": 'Jeson',
+                    "LastName": 'sherpa',
+                    "Gender": 'male',
+                    "DOB": '2019-06-17',
+                    "Phone": '+977-9843384828',
+                    "Address": 'Baneshwor',
+                    "Email": 'pasanglakpasherpa101@gmail.com',
+                    "Password": 'arps'
+                })
+                .end(function(err, res) {
+                    res.should.have.status(200);
+                    res.body.should.be.an('object');
+                    // res.body.should.have.property('message').eql('user data registered');
+                    done();
+                })
+        })
+    })
+})
